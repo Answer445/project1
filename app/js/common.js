428170611500a2f9f6ab1b2ff7e5588aa7928886
Evgeny <evgeni-kolesnikov@mail.ru>
2017-07-12 08:26:23 +0300

    init

---
+++ app/js/common.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".mobile-menu-l, .mobile-menu-r").slideToggle();
+		return false;
+	});
+
+
+	// Custom JS
+
+	// открыть/закрыть мобильное меню
+	
+
+	// открыть закрыть поисковый фильтр
+	
+});
+
+
+
+	window.addEvent('domready',function(){var Slider70 = new DJImageSlider({id: '70', slider_type: 0, slide_size: 900, visible_slides: 1, show_buttons: 0, show_arrows: 0, preload: 800},{auto: 1, transition: Fx.Transitions.Expo.easeInOut, duration: 1000, delay: 4000})});
+
